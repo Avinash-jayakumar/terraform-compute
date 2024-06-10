ac235b93f0ddb4a9395189db9af96129f2187719
Avinash-jayakumar <avinashavi401@gmail.com>
2024-06-10 16:12:41 +0530

    push

--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-#credentials = "${file("terraform-key.json")}"
-project     = "hub-project-418507"
-region = "us-east4"
-}
---
+++ terraform/provider.tf
@@ -0,0 +1,12 @@
+provider "google" {
+#credentials = "${file("terraform-key.json")}"
+project     = "idyllic-slice-422610-p1"
+region = "us-east4"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-backend-wif"
+    prefix  = "terraform/state"
+  }
+}
